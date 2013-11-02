54a068f1eb388d4324761b866c7222de9881e67a
asubota <asubota@cogniance.com>
2013-11-02 23:04:07 +0200

    new json

--- assets/parse.rb
+++ assets/parse.rb
@@ -2,6 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 require "active_support/core_ext"
 require 'open-uri'
+require 'fileutils'
 
 restaurants = []
 
@@ -9,35 +10,56 @@ def get_menu(hash)
   meals = []
 
   restaurant_page = Nokogiri::HTML(open("http://hrum.com.ua/menu/#{hash}")).css('.menu_list').each do |rest|
-    meal_category = rest.css('.menu_btn').text.strip
+      meal_category = rest.css('.menu_btn').text.strip
+      category_meal = rest.css('.menu_togle li').each do |meal|
+        next unless meal.at_css('.pic')
 
-    category_meal = rest.css('.menu_togle li').each do |meal|
-      meal_name = meal.css('.about_box .title a').text.strip
-      meal_text = meal.css('.about_box .text').text.strip
-      meal_price= meal.css('.price_box .price').text
-      key       = meal['id']
-      image     = '' #meal.css('.pic .big_pic')[0]['data']
+        key       = meal['id']
+        meal_name = meal.css('.about_box .title a').text.strip
+        meal_text = meal.css('.about_box .text').text.strip
+        meal_price= meal.css('.price_box .price').text
 
-      m = {
-        key: key,
-        name: meal_name,
-        image: image,
-        description: meal_text,
-        price: meal_price,
-        category: meal_category
-      }
+        meal_hash = meal.css('.pic a')[0]['href'].sub('/menu/', '').split('/')
+        parent    = meal_hash[0]
+        hash      = meal_hash[1]
+      
+        image_path_orig  = meal.css('.pic .big_pic img')[0]['data-original']
+        image_path_local = "../public/media/#{parent}/food/" + image_path_orig.sub('/images/', '')  + '.jpg' 
+        image = image_path_local.sub('../public', '')
 
-      meals.push m
-    end
-  end
+        dirname = File.dirname(image_path_local)
+        unless File.directory?(dirname)
+          FileUtils.mkdir_p(dirname)
+        end
+        
+        open(image_path_local, 'wb') do |file|
+          file << open("http://hrum.com.ua#{image_path_orig}").read
+        end
+      
+        m = {
+          key: key,
+          hash: hash,
+          parent: parent,
+          category: meal_category,
+          image: image,
+          price: meal_price.sub(/[^\d]+/, ''),
+          currency: meal_price.sub(/[\d\s]+грн\./, 'UAH'),
+          name: meal_name,
+          description: meal_text
+        }
 
-  return meals
+        meals.push m
+      end
+    end   
+
+  meals
 end
+
 
 doc = Nokogiri::HTML(open('http://hrum.com.ua/restaurants/all/kiev')).css('.item_box').each do |item|
 
   title = item.css('.title').text.strip
-  key = item.css('.hd_inner a.restaurant_title')[0]['href'].sub('/menu/','')
+  key = item.css('.hd_inner a.restaurant_title')[0]['href'].sub('/menu/', '')
   logo_src = item.css('.item_logo img')[0]['src']
   logo_alt = item.css('.item_logo img')[0]['alt']
 
@@ -50,7 +72,9 @@ doc = Nokogiri::HTML(open('http://hrum.com.ua/restaurants/all/kiev')).css('.item
   min_cost      = item.css('.info_box li')[1].css('span').text
   delivery_time = item.css('.info_box li')[5].css('span').text.sub(/[^\d]+/, '')
   delivery      = item.css('.info_box li')[3].css('span').text.split
-  category      = item.css('.info_box li')[2].css('span').text.split(/[^\w]/)
+  category      = item.css('.info_box li')[2].css('span').text.split(/[,.\\\/]/).map(&:strip)
+
+  puts "exporting #{title}" 
 
   menu = get_menu key
 
@@ -78,13 +102,12 @@ doc = Nokogiri::HTML(open('http://hrum.com.ua/restaurants/all/kiev')).css('.item
       },
       category: category,
       delivery: {
-        min_cost: min_cost.sub('грн.','').strip,
+        min_cost: min_cost.sub('грн.', '').strip,
         time: delivery_time,
         cost: delivery[0],
-        currency: delivery[1].sub('грн.','UAH').strip
+        currency: delivery[1].sub('грн.', 'UAH').strip
       },
       menu: menu
-
   }
 
   restaurants.push r
