a56060a800d97e5ee5c3ebbb167620e928262c17
David <ariedov@gmail.com>
2013-11-02 17:53:25 +0200

    added map to restaurant detailed

--- src/com/example/havkavalera/specify_dialog/SelectRestaurantActivity.java
+++ src/com/example/havkavalera/specify_dialog/SelectRestaurantActivity.java
@@ -1,14 +1,16 @@
 package com.example.havkavalera.specify_dialog;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import com.example.havkavalera.R;
 import com.example.havkavalera.adapters.RestaurantSelectAdapter;
 import com.example.havkavalera.mock.MockRestaurantsList;
 import com.example.havkavalera.model.Restaurant;
-import com.markupartist.android.widget.PullToRefreshListView;
 
 import java.util.List;
 
@@ -16,16 +18,30 @@ public class SelectRestaurantActivity extends Activity {
 
     public static final int REQUEST_CODE = 1002;
 
+    public List<Restaurant> mRestaurants;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_list_layout);
 
         ListView listView = (ListView) findViewById(R.id.selected_list);
-        List<Restaurant> restaurants = MockRestaurantsList.getRestaurants();
-        listView.setAdapter(new RestaurantSelectAdapter(restaurants));
+        mRestaurants = MockRestaurantsList.getRestaurants();
+        listView.setAdapter(new RestaurantSelectAdapter(mRestaurants));
 
         getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Restaurant restaurant = mRestaurants.get(position);
+
+                Intent intent = new Intent(SelectRestaurantActivity.this, DetailedRestaurantActivity.class);
+                intent.putExtra(DetailedRestaurantActivity.RESTAURANT_KEY, restaurant);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
