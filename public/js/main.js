504417a01051ac31ca8434ab210894660c739735
Dmitrii Samoilov <e2718281828@ya.ru>
2013-11-02 14:53:44 +0200

    added showObjectsNear and showObjectsNearByCategory js functions

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 ymaps.ready(init);
-var myMap;
+
+var myMap, placemarks = [];
 
 function init(){     
 	myMap = new ymaps.Map ("map", {
@@ -15,9 +16,49 @@ function show_map(position) {
   var long = position.coords.longitude;
   // let's show a map or do something interesting!
 
-  myMap.panTo([lat, long], {duration: 2000});
+  myMap.panTo([lat, long], {
+		duration: 2000,
+		callback: function () {
+     showObjectsNear(position);
+		}
+	});
 
-	myPlacemark = new ymaps.Placemark([lat, long]);
-	
+	var myPlacemark = new ymaps.Placemark([lat, long]);
+	placemarks.push(myPlacemark);
 	myMap.geoObjects.add(myPlacemark);
+}
+
+function show_venue_on_map(venue) {
+	var placemark = new ymaps.Placemark([venue.lat, venue.long], { content: venue.title, balloonContent: venue.description });
+	placemarks.push(placemark);
+	myMap.geoObjects.add(placemark);
+}
+
+function showObjectsNear(position) {
+	var lat = position.coords.latitude;
+  var long = position.coords.longitude;
+
+	jQuery.post('/location/'+long+'/'+lat, function(resp) {
+		// remove all old placemarks
+		jQuery.each(placemarks, function(placemark) {
+			myMap.geoObjects.remove(placemark);
+		});
+		
+		jQuery.each(resp.objects, function(venue) {
+			show_venue_on_map(venue);
+		});
+	});
+}
+
+function showObjectsNearByCategory(category) {
+	jQuery.post('/restaurants/'+category, function(resp) {
+		// remove all old placemarks
+		jQuery.each(placemarks, function(placemark) {
+			myMap.geoObjects.remove(placemark);
+		});
+
+		jQuery.each(resp.objects, function(venue) {
+			show_venue_on_map(venue);
+		});
+	});
 }
